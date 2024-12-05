694e369ac95fa5f45632c9f6ba51eaa950a2cada
Ping Yin NG <PingYin.NG@aexp.com>
2024-12-04 23:58:27 -0700

    day 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,8 @@
 fun main() {
 //    val day = Day()
 //    val day = Day2()
-    val day = Day3()
+//    val day = Day3()
+    val day = Day4()
     println(day.getAnswer1())
     println(day.getAnswer2())
 }
