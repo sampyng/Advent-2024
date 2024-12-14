acfdaee8fa70a6044d6a8427a6ad476a3a543210
Ping Yin NG <PingYin.NG@aexp.com>
2024-12-13 22:01:32 -0700

    Add all days and inputs

---
+++ src/Day15.kt
@@ -0,0 +1,15 @@
+import java.io.File
+import kotlin.math.round
+
+class Day15 : Day {
+    private val input = File("src/res/input 14.txt").readText()
+
+    override fun getAnswer1(): Any {
+        return 0
+    }
+
+    override fun getAnswer2(): Any {
+        return 0
+    }
+}
+
